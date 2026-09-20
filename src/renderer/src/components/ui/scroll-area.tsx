32aac83ef9dbbe19663cfdc4f2f1e80a4dabb54f
Ewen <ewen@aipoch.com>
2026-09-20 22:38:08 +0700

    fix(renderer): auto-hide idle scrollbars

--- src/renderer/src/components/ui/scroll-area.tsx
+++ src/renderer/src/components/ui/scroll-area.tsx
@@ -16,7 +16,7 @@ function ScrollArea({
   return (
     <ScrollAreaPrimitive.Root
       data-slot="scroll-area"
-      className={cn('relative', className)}
+      className={cn('scrollbar-auto-hide relative', className)}
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
